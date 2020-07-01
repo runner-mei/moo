8f6c350c4ad82e6519eb396b09681bd5419b23c3
meifakun <runner.mei@gmail.com>
2020-07-02 07:32:36 +0800

    fix(tunnel): 修复 tunnel 没有正确启动的问题

--- tunnel/node/node.go
+++ tunnel/node/node.go
@@ -2,23 +2,34 @@ package server
 
 import (
 	"context"
+	"strings"
 
 	"github.com/runner-mei/log"
-	"github.com/runner-mei/moo/tunnel"
 	"github.com/runner-mei/moo"
+	"github.com/runner-mei/moo/tunnel"
+	"github.com/runner-mei/errors"
 	"go.uber.org/fx"
 )
 
 func init() {
 	moo.On(func() moo.Option {
 		return fx.Provide(func(lifecycle fx.Lifecycle, env *moo.Environment, logger log.Logger) (*tunnel.TunnelListener, error) {
+			acceptURL := env.DaemonUrlPath
+			if !strings.HasPrefix(acceptURL, "/") {
+				acceptURL = "/" + acceptURL
+			}
+			if strings.HasSuffix(acceptURL, "/") {
+				acceptURL = acceptURL + "tunnel"
+			} else {
+				acceptURL = acceptURL + "/tunnel"
+			}
 			tunnelListener, err := tunnel.Listen(logger,
-				 env.Config.IntWithDefault("tunnel.max_threads", 10),
-				 env.Config.StringWithDefault("tunnel.remote.network", "tcp"), 
-				 env.Config.StringWithDefault("tunnel.remote.address", ""), 
-				 env.Config.StringWithDefault("tunnel.remote.listen_at_url", ""),)
-			if  err != nil {
-				return nil, err
+				env.Config.IntWithDefault("tunnel.max_threads", 10),
+				env.Config.StringWithDefault("tunnel.remote.network", "tcp"),
+				env.Config.StringWithDefault("tunnel.remote.address", ""),
+				env.Config.StringWithDefault("tunnel.remote.listen_at_url", acceptURL))
+			if err != nil {
+				return nil, errors.Wrap( err, "tunel listen")
 			}
 
 			lifecycle.Append(fx.Hook{
@@ -30,6 +41,12 @@ func init() {
 				},
 			})
 			return tunnelListener, nil
+		})
+	})
+
+	moo.On(func() moo.Option {
+		return fx.Invoke(func(tunnelSrv *tunnel.TunnelListener) error {
+			return nil
 		})
 	})
 }
